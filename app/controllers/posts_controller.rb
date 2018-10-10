137fa564c67fddc00997b11eeaad509ce0307c60
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-10-10 14:52:49 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,15 @@
 class PostsController < ApplicationController
-
+  
   def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+
+  private
+  def set_params
     @post = Post.find(params[:id])
   end
 end
