bb1a8749c3ddcc818315084db2e01bf10d837d06
nichia <nichia@gmail.com>
2018-10-24 19:48:35 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,12 @@
+# app/controllers/posts_controller.rb
+
 class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def index
+    @posts = Post.all
   end
 end
