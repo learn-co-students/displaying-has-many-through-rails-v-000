6288c937c590b804c92a5d8c34baf7bca85abc73
epic0448 <samuelogundiran@Yahoo.com>
2018-12-17 19:42:26 -0500

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
   has_many :comments
   has_many :posts, through: :comments
+
+  # # Instead, we tell Active Record to look 
+  # through the comments table to 
+  # figure out this association by 
+  # declaring that our User has_many :posts, through: :comments
 end
