b1f4b76917c96a1be8f79aaed91240469f290cf0
github username <github email address>
2019-01-17 11:03:09 -0600

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,13 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show]
 
   def show
     @post = Post.find(params[:id])
   end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end 
 end
