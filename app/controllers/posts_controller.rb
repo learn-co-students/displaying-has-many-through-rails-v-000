8b1db7c0e533c0d695144b1e1ac1d9a9d2645b04
Joshua Tan <jtsarvida@gmail.com>
2019-12-04 17:00:21 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,6 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+  
+  # have to change something in order to learn submit, so here's my comment.
 end
